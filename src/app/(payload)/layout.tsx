ada987388111b1e2db65305158ff1ebdc7081a5a
Suhaib Chaudhry <mbpro@Mac.lan>
2026-05-01 15:36:12 -0500

    Revert "Testimonials collection + branded admin theme (v2 with pre-push table drop) (#18)"
    This reverts commit bf3245f1758c577cd920fd9853bcf2c09d25a061.

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -3,8 +3,6 @@
 import React from 'react'
 import config from '@payload-config'
 import '@payloadcms/next/css'
-import './admin-theme.css'
-import { manrope, fraunces, jetbrains } from '@/lib/fonts'
 import type { ServerFunctionClient } from 'payload'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import { importMap } from './admin/importMap'
@@ -17,12 +15,7 @@ const serverFunction: ServerFunctionClient = async function (args) {
 type Props = { children: React.ReactNode }
 
 const Layout = ({ children }: Props) => (
-  <RootLayout
-    config={config}
-    importMap={importMap}
-    serverFunction={serverFunction}
-    htmlProps={{ className: `${manrope.variable} ${fraunces.variable} ${jetbrains.variable}` }}
-  >
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 )
